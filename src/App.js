0c5720f994ebd955243cf7c673cdeeb39dc17a7e
ShemanthDeveloper <hemanthsudhani36@gmail.com>
2023-12-18 09:05:02 +0530

    fixed react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React ,{ Suspense }from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from "./components/NavBar";
 import Contact from './components/Contact';
-import Skills from "./components/Skills";
+
 import Main from './components/Main';
 import About from './components/About';
 import Projects from './components/Projects';
+
 import './App.css';
+const Skills = React.lazy(() => import('./components/Skills'));
 
 function App() {
   return (
@@ -18,7 +21,11 @@ function App() {
       <Routes>
           <Route exact path="/"  element={<Main/>} />
           <Route exact path="/about" element={<About/>} />
-          <Route exact path="/skills" element={<Skills/>} />
+          <Route exact path="/skills" element={<div>
+        <Suspense fallback = { <div> Please Wait... </div>} >
+          <Skills/></Suspense>
+    </div>
+} />
           <Route exact path="/projects" element={<Projects/>} />
           <Route exact path="/contact" element={<Contact/>} />
       
